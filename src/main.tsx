504e8f38c96b9f659ab2752471937166d02ff66c
acerini2023 <127701280+acerini2023@users.noreply.github.com>
2024-03-14 16:53:55 -0400

    creating sass variables

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-import "./tokens/button.css";
-import "./tokens/color-palette.css";
-import "./tokens/input-tokens.css";
-import "./tokens/theme-tokens.css";
-import "./globals.css";
+import "./assets/sass/main.scss";
 
 const root = document.getElementById("root");
 
